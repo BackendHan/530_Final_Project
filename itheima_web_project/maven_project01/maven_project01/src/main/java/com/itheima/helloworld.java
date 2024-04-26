4f480b92da1e36779a83841baee1553b03b959e0
BackendHan <144406286+BackendHan@users.noreply.github.com>
2024-04-25 22:47:43 -0400

    Add files via upload

---
+++ itheima_web_project/maven_project01/maven_project01/src/main/java/com/itheima/helloworld.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.slf4j.Logger;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloMaven!");
+
+    }
+}
---
+++ itheima_web_project/maven_project01/maven_project01/src/test/java/com/itheima/test/DemoTest.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+    }
+}
